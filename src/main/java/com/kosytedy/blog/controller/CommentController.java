aef2a0b93e8385be93aea2e1e7c8ef85dfe2eb8a
Teddy <Teddy@Teddys-MacBook-Pro.local>
2020-09-05 19:11:53 +0100

    Added Comment Service

---
+++ src/main/java/com/kosytedy/blog/controller/CommentController.java
@@ -0,0 +1,11 @@
+package com.kosytedy.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comment")
+public class CommentController {
+
+	
+}
--- src/main/java/com/kosytedy/blog/service/CommentService.java
+++ src/main/java/com/kosytedy/blog/service/CommentService.java
@@ -1,13 +1,46 @@
 package com.kosytedy.blog.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.kosytedy.blog.exception.ResourceNotFoundException;
+import com.kosytedy.blog.model.Comment;
 import com.kosytedy.blog.repository.CommentRepository;
+
+import net.minidev.json.JSONObject;
 
 @Service
 public class CommentService {
 
 	@Autowired
 	CommentRepository commentRepository;
+	
+	public List<Comment> getComments(){
+		return commentRepository.findAll();
+	}
+	
+	public Optional<Comment> getUser(Long commentId){
+		return Optional.of(commentRepository.findById(commentId))
+				.orElseThrow(() -> new ResourceNotFoundException(commentId));
+	}
+	
+	public Comment saveComment(Comment comment) {
+		return commentRepository.save(comment);
+	}
+	
+	public ResponseEntity<?> deleteComment(Long commentId){
+		
+		if(commentRepository.existsById(commentId)) {
+			JSONObject body = new JSONObject();
+			commentRepository.deleteById(commentId);
+			body.put("message", "Comment deleted successfully.");
+			return ResponseEntity.ok(body);
+		}
+		
+		throw new ResourceNotFoundException(commentId);
+	}
 }
